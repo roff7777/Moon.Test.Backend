803449de489ecca4f38f54951ed5fcc4de094968
Edgar Hernandez Martinez <roff7777@gmail.com>
2019-06-28 15:00:15 -0500

    Se termina funcionalidad base, aun hace falta resolver algunos bugs

--- app.js
+++ app.js
@@ -37,9 +37,8 @@ io.on('connection', function(socket){
         console.log('user disconnected');
     });
 
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    socket.on('chat message', function(roomMessage){
+        io.to(roomMessage.room).emit('chat message', roomMessage);
     });
 
     socket.on('join chat', function(conversacion){
